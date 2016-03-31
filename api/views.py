60614cffdce18957af9871e53de56aded0b0c7e0
Tobiasz Parys <tobiasz.parys@eniro.com>
2016-03-31 17:37:22 +0200

    x

--- api/views.py
+++ api/views.py
@@ -29,23 +29,22 @@ def post_list(request):
 	else:
 		ip = request.META.get('REMOTE_ADDR')
 
-	key = "asd"
-	# key = "key.ip:%s" % ip[0]
-	# deeplink = request.GET.get('deeplink', '')
+	key = "key.ip:%s" % ip[0]
+	deeplink = request.GET.get('deeplink', '')
 	
-	# invite = Invite.objects.filter(key=key)
-	# if not invite.exists():
-	# 	invite = Invite()
-	# else :
-	# 	invite = invite[:1].get()
-	# invite.key = key
-	# invite.deeplink = deeplink
+	invite = Invite.objects.filter(key=key)
+	if not invite.exists():
+		invite = Invite()
+	else :
+		invite = invite[:1].get()
+	invite.key = key
+	invite.deeplink = deeplink
 
-	# invite.publish()
+	invite.publish()
 
-	html = "<html><body><pre>key: %s, deeplink: %s</pre></body></html>" % (key, ip)
-	return HttpResponse(html)
-	# return HttpResponseRedirect("https://itunes.apple.com/pl/app/eniro-pa-sjon-free-nautical/id444222894?mt=8")
+	# html = "<html><body><pre>key: %s, deeplink: %s</pre></body></html>" % (key, ip)
+	# return HttpResponse(html)
+	return HttpResponseRedirect("https://itunes.apple.com/pl/app/eniro-pa-sjon-free-nautical/id444222894?mt=8")
 
 class ShipList(generics.ListCreateAPIView):
 	queryset = Ship.objects.all()
